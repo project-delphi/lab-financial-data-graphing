4ff2e0e509a1847c17b9474976ed6480cfb4776e
Ravi Kalia <ravkalia@gmail.com>
2018-02-20 12:48:19 -0600

    skeletion of iterations 1-3

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,68 @@
+const coindeskUrl = "http://api.coindesk.com/v1/bpi/historical/close.json"
+
+window.onload = function() { 
+    const ctx = document.getElementById("canvas").getContext("2d")
+    const axios_data = axiosCall(coindeskUrl)
+    window.myLine = new Chart(ctx, configData(axios_data))
+}
+
+const axiosCall = (my_url) => {
+axios({
+    method: "GET",
+    url: my_url, 
+  })
+  .then(response => {
+    return response.bpi
+  })
+  .catch(err => {
+    err ? console.log(err)
+  })
+}
+ const configData = (obj) => {
+  const {keys, values} = Object.entries(obj)
+  const config = {
+      type: 'line',
+      data: {
+          labels: [keys],
+          datasets: [{
+              label: "My First dataset",
+              backgroundColor: window.chartColors.red,
+              borderColor: window.chartColors.red,
+              data: [values],
+              fill: false,
+          }]
+      },
+      options: {
+          responsive: true,
+          title:{
+              display:true,
+              text:'Chart.js Line Chart'
+          },
+          tooltips: {
+              mode: 'index',
+              intersect: false,
+          },
+          hover: {
+              mode: 'nearest',
+              intersect: true
+          },
+          scales: {
+              xAxes: [{
+                  display: true,
+                  scaleLabel: {
+                      display: true,
+                      labelString: 'Date'
+                  }
+              }],
+              yAxes: [{
+                  display: true,
+                  scaleLabel: {
+                      display: true,
+                      labelString: 'Value'
+                  }
+              }]
+          }
+      }
+  }
+ return config
+}
